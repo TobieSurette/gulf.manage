58013f807a40e54e30a326f61e0271d04fdf3dc2
Tobie Surette <tobie.surette@gmail.com>
2021-04-06 08:32:52 -0300

    Moving 1988 data

---
+++ build/scs 1988/README.md
@@ -0,0 +1,39 @@
+# Southern Gulf of Saint Lawrence Snow Crab Survey - 1988 
+
+Taken from: 
+
+Moriyasu, M., Chiasson, Y., Wade E., DeGrace, P. and Conan, G. 1989. Prince Edward Island Snow Crab, *Chionoecetes opilio* Fishery Stock Assessment - 1988 Fall Season. Canadian Atlantic Fisheries Scientific Advisory Committee, Research Document 89/27:20pp.
+
+A post season trawl survey was conducted on the Southern Gulf of St. Lawrence between August 5th and October 3rd, 1988. 
+
+A standard 20m Nephrops trawl with SCANMAR electronic net sensor was used on a chartered vessel for the research survey. 
+
+Stations were sampled on a twelve hour basis during daylight. 
+
+The duration of the tows varied from 4 to 8 minutes at a speed of 2.0-3.5 knots. 
+
+A standard trawl haul starts when the predetermined amount of cable (usually three times the depth) is let out and the winch drums are locked. 
+
+The swept surface by trawl net was estimated from the data on net wing width measured by SCANMAR electronic net sensor
+
+The catch is sorted out by sex,size, morphometric maturity, molt stages and the presence/absence of the external eggs for females .
+
+This year's survey was planned in order to cover as large an area as possible. Therefore the number of tows per 10'x10' grid was limited to one. 
+
+Carapace condition | Description
+------------------ | --------------------------
+ 1                 | New-shelled crab.
+ 2                 | Intermediate-age shelled crab.
+ 3                 | Old-shelled crab.
+
+## Snow Crab Survey Build Functions:
+
+Function            | Description
+------------------- | --------------------------
+`scs.set.1988`      | Build snow crab survey set data for 1988.
+`scs.cat.1988`      | Build snow crab survey by-catch data for 1988.
+`scs.bio.1988`      | Build snow crab survey crab biological data for 1988.
+`scs.scanmar.1988`  | Build snow crab survey crab Scanmar acoustic trawl data for 1988.
+`scs.metadata.1988` | Build snow crab survey crab metadata for 1988.
+
+
--- build/scs 1988/scs.set.1988.R
+++ build/scs 1988/scs.set.1988.R
@@ -1,7 +1,10 @@
 library(gulf.data)
 library(gulf.spatial)
 
-# Read raw data:
+# Flag to publish data: 
+publish <- FALSE
+
+# Read original raw set data:
 x <- read.csv(locate(file = c("scs.set", "1988", "csv")), header = TRUE)
 
 # Variable name fix:
@@ -42,7 +45,43 @@ x$latitude.start.logbook[is.na(x$latitude.start.logbook)]   <- x$lorany.start[is
 x$longitude.end.logbook[is.na(x$longitude.end.logbook)]     <- x$loranx.end[is.na(x$longitude.end.logbook)]
 x$latitude.end.logbook[is.na(x$latitude.end.logbook)]       <- x$lorany.end[is.na(x$latitude.end.logbook)] 
 
-# Fix time fields:
+# Extract sampler and coordiante data from biological data:
+files <- locate(file = "GCR88")
+b <- read.scsbio(files, drop = FALSE)
+r <- aggregate(b["longitude.start"], by = b[c("date", "tow.number")], unique)
+r$latitude.start <- aggregate(list(x = b$latitude.start), by = b[c("date", "tow.number")], unique)$x
+r$sampler <- aggregate(list(x = b$samplers), by = b[c("date", "tow.number")], function(x) paste(unique(x), collapse = ","))$x
+r <- sort(r, by = c("date", "tow.number"))
+r$sampler <- sampler(r$sampler, project = "scs")
+r$sampler[r$date == "1988-11-09" & r$tow.number == 8] <- "HELENE CHIASSON, RENALD HACHE"
+r$sampler[r$date == "1988-08-23" & r$tow.number == 3] <- "MARCEL HEBERT, RENALD HACHE"
+ix <- match(x[c("date", "tow.number")], r[c("date", "tow.number")])
+x$sampler <- r$sampler[ix]
+x$sampler[is.na(x$sampler)] <- ""
+x$latitude <- r$longitude.start[ix]
+x$longitude  <- r$latitude.start[ix]
+
+# Spot coordinate corrections:
+x$longitude.end.logbook[which((x$date == "1988-08-09") & (x$tow.number == 6))] <- 14929.6
+x$latitude.start.logbook[which((x$date == "1988-08-09") & (x$tow.number == 6))] <- 29917.2
+x$latitude.start.logbook[which((x$date == "1988-09-17") & (x$tow.number == 2))] <- 29626.4
+x$latitude.start.logbook[which((x$date == "1988-08-07") & (x$tow.number == 6))] <- 30298.5
+x$latitude.start.logbook[which((x$date == "1988-11-13") & (x$tow.number == 5))] <- 30720.1  
+x$latitude.end.logbook[which((x$date == "1988-08-21") & (x$tow.number == 3))] <- 31029.4 
+x$latitude.end.logbook[which((x$date == "1988-09-02") & (x$tow.number == 2))] < 29335.9
+x$latitude.start.logbook[which((x$date == "1988-08-09") & (x$tow.number == 9))] <- 29644.0
+x$latitude.start.logbook[which((x$date == "1988-08-31") & (x$tow.number == 1))] <- 30151.6
+x$longitude.end.logbook[which((x$date == "1988-08-11") & (x$tow.number == 3))] <- 14963.3
+x$longitude.end.logbook[which((x$date == "1988-08-22") & (x$tow.number == 3))] <- 15139.0
+x$latitude[which((x$date == "1988-08-23") & (x$tow.number == 1))] <- 30239.5 
+x$latitude[which((x$date == "1988-08-10") & (x$tow.number == 3))] <- NA   
+x$longitude[which((x$date == "1988-08-10") & (x$tow.number == 3))] <- NA 
+
+# Replace redundant coordinates:
+x$longitude[which(!is.na(x$longitude.start.logbook) | !is.na(x$longitude.end.logbook))] <- NA
+x$latitude[which(!is.na(x$latitude.start.logbook) | !is.na(x$latitude.end.logbook))] <- NA
+
+# Format time fields:
 ix <- which(!is.na(x$start.time.logbook) & (nchar(x$start.time.logbook) == 4))
 x$start.time.logbook[ix] <- paste0("0", x$start.time.logbook[ix])
 ix <- which(nchar(x$start.time.logbook) == 5)
@@ -58,12 +97,52 @@ x$end.time.logbook[which(is.na(x$end.time.logbook) | x$end.time.logbook == "")] 
 
 # Fix comments:
 x$comment <- deblank(x$comment)
+x$comment <- gsub("[Mm]\x88tre[s]*", "m ", x$comment)
+x$comment <- gsub("[Mm]\x88[s]*", "meme ", x$comment)
 
-# Remove empty variables:
-x <- squeeze(x)
+# Extract tow duration information from comments:
+ix <- grep("[0-9]*[:]*[0-9][ ]*minu", tolower(x$comment))
+str <- x$comment[ix]
+str <- deblank(unlist(lapply(strsplit(str, "minu"), function(x) x[1])))
+str <- unlist(lapply(strsplit(str, " "), function(x) x[length(x)]))
+str[gsub("[:]", "", str) == str] <- paste0(str[gsub("[:]", "", str) == str], ":00")
+x$duration <- ""
+x$duration[ix] <- str
+
+# Extract wing spread information from comments:
+ix <- grep("[0-9]+[.;,][0-9]", tolower(x$comment))
+x$comment <- gsub("7.5 ms", "7.5m", x$comment)
+x$comment <- gsub("6;4[ ms]", "6.4m ", x$comment)
+x$comment <- gsub("9;2[ ms]*", "9.2m ", x$comment)
+x$comment <- gsub("5;9[ ms]*", "5.9m ", x$comment)
+x$comment <- gsub("5;7[ ms]*", "5.7m ", x$comment)
+x$comment <- gsub(" 7[ ms]*", " 7.0m ", x$comment)
+x$comment <- gsub("6;0[ ms]*", " 6.0m ", x$comment)
+x$comment <- gsub("6;4[ ms]*", " 6.4m ", x$comment)
+x$comment <- gsub("5;4[ ms]*", " 5.4m ", x$comment)
+x$comment <- gsub("5;0[ ms]*", " 5.0m ", x$comment)
+x$comment <- gsub("6;3[ ms]*", " 6.3m ", x$comment)
+x$comment <- gsub("5;5[ ms]*", " 5.5m ", x$comment)
+x$comment <- gsub("5;3[ ms]*", " 5.3m ", x$comment)
+x$comment <- gsub("6;1[ ms]*", " 6.1m ", x$comment)
+x$comment <- gsub("4;0[ ms]*", " 4.0m ", x$comment)
+x$comment <- gsub("8;2[ ms]*", " 8.2m ", x$comment)
+x$comment <- gsub("9;0[ ms]*", " 9.0m ", x$comment)
+x$comment <- gsub("9;3[ ms]*", " 9.3m ", x$comment)
+x$comment <- gsub("3;3[ ms]*", " 3.3m ", x$comment)
+x$comment <- gsub("1;8[ ms]*", " 1.8m ", x$comment)
+x$comment <- gsub("2;3[ ms]*", " 2.3m ", x$comment)
+x$comment <- gsub("8;0[ ms]*", " 8.0m ", x$comment)
+x$comment <- gsub("8;3[ ms]*", " 8.3m ", x$comment)
+x$comment <- gsub("10;0[ ms]*", "10.0m ", x$comment)
 
 # Remove redundant fields:
 remove <- c("year", "month", "day", "season", "vessel", "loranx.start", "loranx.end", "lorany.start", "lorany.end")
 x <- x[setdiff(names(x), remove)]
+
+# Remove empty variables:
+x <- squeeze(x)
+
+x$duration
 
 
