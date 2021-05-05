eed113abc3c00ac5bc03f174ce40b355e7eeeb89
Tobie Surette <tobie.surette@gmail.com>
2021-05-05 16:24:59 -0300

    Minor data file fixes

--- build/scsbio.R
+++ build/scsbio.R
@@ -1,17 +1,24 @@
 library(gulf.data)
 
-year <- 1990  # Survey year.
+year <- 1988  # Survey year.
 
 # Extract sampler and coordinate data from biological data:
 files <- locate(file = paste0("GCR", substr(year, 3,4)))
-b <- read.scsbio(files, drop = FALSE)
+b <- read.scsbio(files, drop = FALSE, verbose = TRUE)
 
+# Process numerical codes:
+b$eggs.remaining <- gsub("[*]", "", b$eggs.remaining)
+b$comments <- gsub("[*]", "", b$comments)
+b$comments <- gsub(" +", " ", b$comments)
+names(b) <- gsub("comments", "comment", names(b))
+b$durometer <- gsub("[*]", "", b$durometer)
+b$durometer <- as.numeric(b$durometer)
 b <- b[, -grep("tude", names(b))]
 b <- b[, -grep("position", names(b))]
 b <- b[, -grep("samplers", names(b))]
 b <- b[, -grep("maturity", names(b))]
 b <- b[, -grep("depth", names(b))]
-b <- b[, -grep("durometer", names(b))]
+b <- b[, -grep("zone", names(b))]
 b <- compress(b)
 
 # Write data to 'gulf.data':
